7cd8afb0e61c11bfcbd731b14caa716240ef1274
natashasingh2018 <natashasingh2018@users.noreply.github.com>
2017-05-09 09:52:23 -0500

    class commit

---
+++ app/models/director.rb
@@ -0,0 +1,13 @@
+class Director < ApplicationRecord
+
+has_many :movies, :class_name => "Movie", :foreign_key => "director_id"
+
+  # Director:
+  #  - name: must be present; must be unique in combination with dob
+
+  validates :name, :presence => true, :uniqueness => {:scope => :dob}
+  #  - dob: no rules
+  #  - bio: no rules
+  #  - image_url: no rules
+
+end
